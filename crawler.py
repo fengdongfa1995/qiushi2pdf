12cf74e8f11b68a9f74fa282860e8cc5e02dc1c2
StableGenius <feng@dongfa.pro>
2022-06-16 23:44:08 +0800

    finish main function

--- crawler.py
+++ crawler.py
@@ -17,38 +17,59 @@ class QiuShiCrawler:
         self.img_src_re = re.compile(r'src="(.*?)"')
         self.font_tag_re = re.compile(r'<[/]?font.*?>')
 
-        # xpath
-        self.title_xpath = "string(//div[@class='highlight']/p[1])"
-        self.author_xpath = "string(//div[@class='highlight']/p[2])"
-        self.content_xpath = "//div[@class='highlight']/p[position()>2]"
+        # xpath for extracting paper title
+        self.title_xpath = "//h1/text()"
+        # xpath for extracting paper volume
+        self.volume_xpath = "//span[@class='appellation'][1]/text()"
+        # xpath for extracting paper author
+        self.author_xpath = "//span[@class='appellation'][2]/text()"
+        # xpath for extracting paper contents, not empty paragraph or have a child
+        self.content_xpath = "//div[@class='highlight']/p[text() or *]"
 
     def fetch_info(self, url: str) -> dict:
         """fecth key information from qiushi.com"""
         result = {'content': []}
 
-        # download html from internet
+        # get source code of html
         resp = self.session.get(url=url)
         resp.encoding = 'utf-8'
-
-        # extract paper content from html source code
         html = etree.HTML(resp.text)
 
-        # get title and author of paper
-        result['title'] = html.xpath(self.title_xpath)
-        result['author'] = html.xpath(self.author_xpath)
+        # extract title from html source code
+        paper_title = html.xpath(self.title_xpath)[0].strip()
+        paper_title = re.sub(r'\s+', ' ', paper_title)
+        result['title'] = paper_title
+
+        # extract volume from html source code
+        paper_volume = html.xpath(self.volume_xpath)[0].strip()[3:]  # drop '来源：' from string
+        result['volume'] = paper_volume
+
+        # extract author from html source code
+        paper_author = html.xpath(self.author_xpath)[0].strip()[3:]  # drop '作者：' from string
+        result['author'] = paper_author
+
 
         # get content of paper
         block = {}
         for paragraph in html.xpath(self.content_xpath):
+            # skip everything before author (include author itself)
+            content = paragraph.xpath('string(.)').strip()
+            if content == paper_author:
+                result['content'] = []
+                continue
+
             raw_code = etree.tostring(paragraph, encoding='utf-8').decode('utf-8')
+            # just keep content between <p> and </p>
             content = self.paragraph_re.search(raw_code).group(1).strip()
+            # drop <font> and </font>
             content = self.font_tag_re.sub("", content).strip()
             
             if content.startswith('<img'):
+                # if this tag is a img
                 img_url = self.img_src_re.search(content).group(1)
                 block['img_url'] = urljoin(url, img_url)
             else:
-                block['text'] = content
+                block['text'] = content.replace(' ', r'\ ')
                 result['content'].append(block)
                 block = {}
 
@@ -59,7 +80,7 @@ def main():
     """test function"""
     app = QiuShiCrawler()
 
-    target_url = 'http://www.qstheory.cn/dukan/qs/2022-06/01/c_1128695652.htm'
+    target_url = 'http://www.qstheory.cn/dukan/qs/2022-06/15/c_1128739416.htm'
     info = app.fetch_info(target_url)
     print(info)
 
